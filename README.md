da9d3006851f5b4fb5ebd9b6c56505b5727c77be
EtaoinWu <etaoin.wu@gmail.com>
2022-12-06 02:14:49 +0800

    init ver

---
+++ README.md
@@ -0,0 +1,3 @@
+# Notibot
+
+Accepts webhook requests from [Filter Box](http://filterbox.catchingnow.com/Webhook_%E5%8A%9F%E8%83%BD%E6%8C%87%E5%8D%97.html), and sends it via telegram.
---
+++ main.py
@@ -0,0 +1,75 @@
+from configparser import ConfigParser
+import asyncio
+import pyrogram
+from pyrogram import Client, filters, enums
+from aiohttp import web
+from datetime import datetime
+import html
+
+config = ConfigParser()
+config.read("config.ini")
+
+webhook_host = config["webhook"]["hostname"]
+webhook_port = config.getint("webhook", "port")
+
+proxy_dict = None
+if config.has_section("proxy"):
+    proxy_dict = {
+        "scheme": config["proxy"]["scheme"],
+        "hostname": config["proxy"]["hostname"],
+        "port": int(config["proxy"]["port"]),
+        "username": config["proxy"].get("username", None),
+        "password": config["proxy"].get("password", None),
+    }
+    if proxy_dict["username"] is None:
+        del proxy_dict["username"]
+    if proxy_dict["password"] is None:
+        del proxy_dict["password"]
+
+
+async def main():
+    tg = Client(
+        "my_bot",
+        api_id=config["pyrogram"]["api_id"],
+        api_hash=config["pyrogram"]["api_hash"],
+        bot_token=config["pyrogram"]["bot_token"],
+        proxy=proxy_dict,
+    )
+
+    @tg.on_message(filters.command("getid"))
+    async def getid(client, message):
+        await message.reply_text(message.chat.id)
+
+    http = web.Application()
+    routes = web.RouteTableDef()
+
+    @routes.get("/")
+    async def hello(request):
+        return web.Response(text="Hello, world")
+
+    @routes.post("/new_notification")
+    async def new_notification(request):
+        json = await request.json()
+        title = json["title"]
+        text = json["text"]
+        app = json["app"]
+        time = datetime.fromtimestamp(int(json["time"]) // 1000)
+        message = f"<b>{html.escape(title)}</b>\n{html.escape(text)}\n<i>{html.escape(app)},</i> <code>{time}</code>"
+        print(message)
+        await tg.send_message(
+            config["pyrogram"]["chat_id"], message, parse_mode=enums.ParseMode.HTML
+        )
+        return web.Response(text="OK")
+
+    http.add_routes(routes)
+    runner = web.AppRunner(http)
+    await runner.setup()
+    site = web.TCPSite(runner, webhook_host, webhook_port)
+    await site.start()
+
+    await tg.start()
+    await pyrogram.idle()
+    await tg.stop()
+
+
+asyncio.run(main())
