6f0703912515066b4a50a359d87cadbbbb9038d7
EtaoinWu <etaoin.wu@gmail.com>
2022-12-21 15:22:36 +0800

    add: push notification support for pushsafer

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ from configparser import ConfigParser
 import asyncio
 import pyrogram
 from pyrogram import Client, filters, enums
+import aiohttp
 from aiohttp import web
 from datetime import datetime
 import html
 
 
 config = ConfigParser()
-config.read("config.ini")
+config.read("config.ini", encoding="utf-8")
 
 webhook_host = config["webhook"]["hostname"]
 webhook_port = config.getint("webhook", "port")
+
+pushsafer_key = config["pushsafer"].get("private_key", None)
+pushsafer_icon_map = config["pushsafer_icon_map"]
 
 proxy_dict = None
 if config.has_section("proxy"):
@@ -29,6 +33,8 @@ if config.has_section("proxy"):
 
 
 async def main():
+    pushsafer_session = aiohttp.ClientSession('https://www.pushsafer.com')
+
     tg = Client(
         "my_bot",
         api_id=config["pyrogram"]["api_id"],
@@ -60,6 +66,19 @@ async def main():
         await tg.send_message(
             config["pyrogram"]["chat_id"], message, parse_mode=enums.ParseMode.HTML
         )
+        if app in pushsafer_icon_map and pushsafer_key is not None:
+            async with pushsafer_session.post(
+                "/api",
+                data={
+                    "k": pushsafer_key,
+                    "t": f"[{app}] {title}",
+                    "m": text,
+                    "i": pushsafer_icon_map.get(app, 1),
+                    "d": "a",
+                    "s": "0",
+                },
+            ) as resp:
+                print(resp.status)
         return web.Response(text="OK")
 
     http.add_routes(routes)
